17ed8e4811fab21c31ad8aef5c97bced1e2a9108
yuliangliang <yuliang1345@163.com>
2017-03-18 11:17:38 +0800

    静态库

---
+++ classes/YLLTestLibrary.h
@@ -0,0 +1,15 @@
+//
+//  YLLTestLibrary.h
+//  YLLTestLibrary
+//
+//  Created by 于亮亮 on 2017/3/18.
+//  Copyright © 2017年 com.liang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YLLTestLibrary : NSObject
+
++ (void)testSsomething;
+
+@end
